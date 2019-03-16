1df6b7be76d6b66e88e56be5aba3ac87a064d908
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MacBook-Pro.local>
2019-03-15 19:32:07 -0700

    (init): initial commit

---
+++ bible_numerology.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s| 
+  s.name        = 'bible_numerology'
+  s.version     = '1.0.1'
+  s.date        = '2019-03-15'
+  s.summary     = ""
+  s.description = ""
+  s.authors     = ["Kendra Kuivenhoven"]
+  s.email       = 'kkuivenhoven@mail.csuchico.edu'
+  s.files       = ["lib/bible_numerology.rb"]
+  s.homepage    = 'http://rubygems.org/gems/bible_numerology'
+  s.license     = 'N/A'
+  s.metadata    = { "source_code_uri" => "" }
+end
---
+++ lib/bible_numerology.rb
@@ -0,0 +1,37 @@
+require 'csv'
+require 'open-uri'
+
+class BibleNumerology
+	@allNumbers = Hash.new
+
+	def initialize(number, interpretation)
+		@number = number
+		@interpretation = interpretation
+	end
+
+	def self.init
+		path = "https://raw.githubusercontent.com/kkuivenhoven/BibleNumerology/master/nums.csv"
+		CSV.new(open(path)).each do |numLine|
+			@allNumbers[numLine[0].to_i] = numLine[1]
+		end
+		return "init complete"
+	end
+
+	def self.getAllNums
+		return @allNumbers.keys
+	end	
+
+	def self.getAllMeaningsOnly
+		return @allNumbers.values.map! { |e| e.gsub("_", " ") }
+	end	
+
+	def self.getAllNumsMeanings
+		return @allNumbers
+	end	
+
+	def self.getNum(num)
+		return @allNumbers[num].gsub("_", " ")
+	end	
+
+end
+
